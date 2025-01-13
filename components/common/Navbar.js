64bb351b48057fed06a4f59c81985fa81bf271a9
Bodemola <bodemola123@gmail.com>
2025-01-13 16:07:42 +0100

    Trying navbar since oh

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -1,22 +1,43 @@
+'use client'
 import React from 'react'
+import { HiOutlineHome } from "react-icons/hi";
+import { LuClipboardList, LuLockKeyhole, LuSettings, LuShoppingCart } from "react-icons/lu";
+import { RxAvatar } from "react-icons/rx";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 
 const Navbar = () => {
   return (
     <div className="h-screen w-16 bg-[#131314] flex flex-col justify-between items-center py-12 text-white">
         <div className='flex flex-col gap-8 justify-center items-center'>
-            <div className='bg-[#000000] border-[0.5px] rounded-[12px] w-8 flex items-center justify-center text-[20px] font-black text-[#C088fb]'>V</div>
+            <button className='bg-[#000000] border-[0.5px] rounded-[12px] w-8 flex items-center justify-center text-[20px] font-black text-[#C088fb]'>V</button>
             <div className='flex flex-col justify-center items-center gap-4'>
-                <div className='p-4 flex justify-center items-center gap-2.5 rounded-[123px] hover:bg-[#C088fb] group'></div>
-                <div className='p-4 flex justify-center items-center gap-2.5 rounded-[123px] hover:bg-[#C088fb]'></div>
-                <div className='p-4 flex justify-center items-center gap-2.5 rounded-[123px] hover:bg-[#C088fb]'></div>
-                <div className='p-4 flex justify-center items-center gap-2.5 rounded-[123px] hover:bg-[#C088fb]'></div>
+            <TooltipProvider>
+  <Tooltip>
+    <TooltipTrigger><button className='p-2 flex justify-center items-center gap-2.5 rounded-[123px] hover:bg-[#C088fb] group'><HiOutlineHome className='text-[20px] text-[#C088fb] group-hover:text-[#f4f4f4]' /></button></TooltipTrigger>
+    <TooltipContent side="right" align="start" sideOffset={10}>
+    <p>Home</p>
+    </TooltipContent>
+  </Tooltip>
+</TooltipProvider>
+                
+                <button className='p-2 flex justify-center items-center gap-2.5 rounded-[123px] hover:bg-[#C088fb] group'><LuClipboardList  className='text-[20px] text-[#C088fb] group-hover:text-[#f4f4f4]'/> </button>
+                <button className='p-2 flex justify-center items-center gap-2.5 rounded-[123px] hover:bg-[#C088fb] group'><LuShoppingCart  className='text-[20px] text-[#C088fb] group-hover:text-[#f4f4f4]'/></button>
+                <button className='p-2 flex justify-center items-center gap-2.5 rounded-[123px] hover:bg-[#C088fb] group'><LuLockKeyhole  className='text-[20px] text-[#C088fb] group-hover:text-[#f4f4f4]'/></button>
             </div>
         </div>
-        <div>
-
+        <div className='flex flex-col gap-4 justify-center items-center'>
+          <button className='p-2 flex justify-center items-center gap-2.5 rounded-[123px] hover:bg-[#C088fb]'><LuSettings className='text-[20px] text-[#f4f4f4]'/></button>
+          <button className='flex justify-center items-center gap-2.5'><RxAvatar className='text-[28px]'/></button>
         </div>
   </div>
   )
 }
 
 export default Navbar
+
--- components/ui/tooltip.jsx
+++ components/ui/tooltip.jsx
@@ -2,13 +2,10 @@
 
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
-
 import { cn } from "@/lib/utils"
 
 const TooltipProvider = TooltipPrimitive.Provider
-
 const Tooltip = TooltipPrimitive.Root
-
 const TooltipTrigger = TooltipPrimitive.Trigger
 
 const TooltipContent = React.forwardRef(({ className, sideOffset = 4, ...props }, ref) => (
@@ -16,11 +13,15 @@ const TooltipContent = React.forwardRef(({ className, sideOffset = 4, ...props }
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      "z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      "z-50 overflow-hidden flex gap-2.5 text-[#ffffff] text-center font-medium rounded-[13px] bg-[#131314] absolute border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md",
+      "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95",
+      "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
       className
     )}
-    {...props} />
+    {...props}
+  />
 ))
+
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
 export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
